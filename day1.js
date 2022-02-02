a757610d9b01e15dc38c4a81a6b924e4c8728e1d
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-02-01 22:55:00 -0800

    Add new exercise

---
+++ day1.js
@@ -0,0 +1,8 @@
+/* This ode results in an error. What do we need to change to 
+make the code work as expected? */
+
+let programmingLanguage = 'JavaScript'; // change const to let to allow re-declaration of variables
+programmingLanguage = 'TypeScript';
+console.log(`The language I am currently using is ${programmingLanguage}`);
+
+
